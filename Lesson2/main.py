16e3abeda1cf3ef3c39680a1685817c5009def21
Bedarbis <glaukevicius@yahoo.com>
2024-08-05 19:20:52 +0300

    adding lesson2

---
+++ Lesson2/main.py
@@ -0,0 +1,20 @@
+# my_list = [] #
+
+# name = "Tom"
+# my_list.append(name)
+# my_list.append(name)
+# my_list.append(name)
+# my_list.append(name)
+# print(my_list)
+
+# my_list = [33, 22, 61, -1]
+# print(min(my_list))
+
+# my_list = [33, 22, 61, -1]
+# print(max(my_list))
+
+my_list = [1, 2 ,3]
+
+for item in my_list
+    print(item)
+    print(f"item + 1 = {item+1}")
